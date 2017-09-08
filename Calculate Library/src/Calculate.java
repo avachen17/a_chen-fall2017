4c34456fd589f8d6af456081774a1d57bf71f50c
avachen17 <31107030+avachen17@users.noreply.github.com>
2017-09-07 22:14:46 -0700

    a few changes....

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,49 +4,59 @@
  * It contains a series of methods to do basic math fractions
  */
 public class Calculate {
-	// square the input
+	// this method finds the square of the input
 	public static int square (int operand) {
 		int squared = operand*operand;
 		return squared; 
 	}
-	//cube the input
+	//this method finds the cube of the input
 	public static int cube (int operand) {
 		int cube = operand*operand*operand; 
 		return cube; 
 	}
-	//average of two numbers
+	//this method finds the average of two numbers
 	public static double average (double operand1, double operand2) {
 		double average = (operand1 + operand2)/2;
 		return average;
 	}
-	//average of three numbers
+	//this method finds the average of three numbers
 	public static double average (double operand1, double operand2, double operand3) {
 		double average = (operand1 + operand2 + operand3)/3;
 		return average; 			
 	}
-	//convert radians to degrees 
+	//this method converts the radians to degrees 
 	public static double toDegrees (double angleRadians) {
 		return angleRadians*(180/3.14159);
 	}
-	//convert degrees to radians 
+	//this method converts degrees to radians 
 	public static double toRadians (double angleDegrees) {
 		return angleDegrees*(3.14159/180); 
 	}
-	//discriminant
-	public static double discriminant (double a, double b, double c) {
-		return ((b*b)- (4*a*c)); 
+	//this method finds the discriminant
+	public static double discriminant (double number1, double number2, double number3) {
+		return ((number2*number2)- (4*number1*number3)); 
 	}
-	//improper fractions 
+	//this method finds improper fractions 
 	public static String toImproperFrac (int sideNumber, int topNumber, int bottomNumber) {
 		return ((sideNumber * bottomNumber) + "/" + (bottomNumber));
 	}
-	//Mixed Numbers
+	//this method finds Mixed Numbers
 	public static String toMixedNum (int top, int bottom) {
 		return (top/bottom) + "_" + (top % bottom) + "/" + (bottom);
 		}
-	//Foil
-	public static String foil (int num1, int num2, int num3, int num4, String v) {
-		return (num1*num3) + v + "^2 +" + ((num1*num4) + (num2*num3)) +v+ "+"+ (num2*num4); 	
+	//this method does Foil method 
+	public static String foil (int num1, int num2, int num3, int num4, String variable) {
+		return (num1*num3) + variable + "^2 +" + ((num1*num4) + (num2*num3)) +variable+ "+"+ (num2*num4); 
 	}
+	//this method determines whether or not one integer is evenly divisible by the other 
+	public static int isDivisibleBy (int number1, int number2) {
+		if (number1%number2 == 0) {
+			return true;
+			else {
+				return false; 
+			}
+		}
+	}
+	
 	}
 
