d199cc0bbfb1c26b25560591d93b95a4ee89b814
avachen17 <31107030+avachen17@users.noreply.github.com>
2018-10-26 11:38:10 -0700

    via computer
    Arrays Mini Lab

---
+++ Array MiniLab/src/LotsofCopies.java
@@ -0,0 +1,46 @@
+//Ava Chen
+//APCS Per2
+//November 1, 2017 
+//Array Mini-Lab
+import java.util.Arrays;
+public class LotsofCopies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int num = 7; 
+	//assigning num to a variable
+	//int num2 = num;
+	//giving the new variable a different value 
+	//num = 3;
+	//System.out.println(num2);
+	String strMain = "APCS";
+	//assigning strMain to a variable
+	//String strMain2 = strMain;
+	//assigning strMain to a different string value 
+	//strMain = "AP Computer Science";
+	//System.out.println(strMain2);
+	int[] arrMain = {1, 2, 3, 4, 5};
+	//assigning arrMain to a different variable 
+	//int [] array2 = arrMain;
+	//changing the value of the new variable 
+	//arrMain [0] = 10;
+	//System.out.println(Arrays.toString(array2));
+	changeMe(num, strMain, arrMain);
+	//System.out.println(num);
+	//System.out.println(strMain);
+	//System.out.println(Arrays.toString(arrMain));
+	}
+	public static void changeMe(int x, String str, int [] arr){
+		//part one 
+		//System.out.println("in changeMe, before the change, (aka x);" + x);
+		 	//x = 25;
+		 	//str = "hello there";
+			//System.out.println("in changeMe, before the change, (aka str);" + str);
+		//System.out.println("in changeME, before the change, arrMain (aka arr):" + Arrays.toString(arr));
+	 	//arr [0] = 12;
+		//System.out.println("In changeMe, after the change, arrMain(aka arr):" + Arrays.toString(arr));
+			//System.out.println("in changeMe, before the change, (aka arr);" + arr);
+//part two is in the main string 
+		
+}
+}
